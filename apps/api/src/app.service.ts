1d37fc4b2f642d273d148ada19d992c23342d2aa
Dschunge <schaar.juergen@gmail.com>
2024-07-31 07:52:36 -0600

    commit 5

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Diria HMS Server is running';
+    return 'Diria HMS Server is running fine';
   }
 }
